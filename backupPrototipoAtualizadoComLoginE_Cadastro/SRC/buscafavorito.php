af72d349d033854a95b20577861ce9314d305e86
AnaJuliaJeronymoFernandes <83045177+AnaJuliaJeronymoFernandes@users.noreply.github.com>
2023-04-03 23:18:23 -0300

    Update buscafavorito.php

--- backupPrototipoAtualizadoComLoginE_Cadastro/SRC/buscafavorito.php
+++ backupPrototipoAtualizadoComLoginE_Cadastro/SRC/buscafavorito.php
@@ -15,25 +15,25 @@
     <main class="resultadobusca">
         <div class="sidebar ajeitaSidebar">
             <div class="conteudo">
-            <img src="assets/img/Bruna.jpg" class="image" alt="">
+            <img src="../assets/img/Bruna.jpg" class="image" alt="">
             <h2>Bruna</h2>
-            <a href="perfilpecas.html"><span class="material-symbols-outlined">person</span>Perfil</a>
-            <a href="favoritos.html"><span class="material-symbols-outlined">star</span>Favoritos</a>
+            <a href="perfilpecas.php"><span class="material-symbols-outlined">person</span>Perfil</a>
+            <a href="favoritos.php"><span class="material-symbols-outlined">star</span>Favoritos</a>
             <a href=""><span class="material-symbols-outlined">checkroom</span>Cadastrar Loja</a>
             <a href=""><span class="material-symbols-outlined">settings</span>Configurações</a>
-            <a href="index.html" id="sair"><span class="material-symbols-outlined">logout</span>Sair</a>
+            <a href="../public/index.php" id="sair"><span class="material-symbols-outlined">logout</span>Sair</a>
             </div>
         </div>
         <div id="feed">
             <h2>Resultados para "Bermuda"</h2>
             <div class="post">
                 <div class="post-header">
-                    <img src="assets/img/Gabriel.jpg">
+                    <img src="../assets/img/Gabriel.jpg">
                     <p>Gabriel Albino</p>
                     <ion-icon name="add-circle-outline"></ion-icon>
                 </div>
                 <div class="post-image">
-                    <img src="assets/img/bermuda.jpg">
+                    <img src="../assets/img/bermuda.jpg">
                 </div>
                 <div class="post-footer">
                     <div class="post-description">
@@ -58,10 +58,26 @@
             
     </main>
 
-        
-        <script src="assets/js/script.js"></script>
+        <script src="../assets/js/script.js"></script>
         
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
+    
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php
+    include("../include/rodape.php");
+?>
 </html>
