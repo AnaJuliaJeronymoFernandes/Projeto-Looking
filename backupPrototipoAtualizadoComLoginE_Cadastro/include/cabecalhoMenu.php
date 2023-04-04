8fa198ed37ecb0c5a60cce956913bdaaca746a7f
AnaJuliaJeronymoFernandes <83045177+AnaJuliaJeronymoFernandes@users.noreply.github.com>
2023-04-03 23:02:49 -0300

    Create cabecalhoMenu.php

---
+++ backupPrototipoAtualizadoComLoginE_Cadastro/include/cabecalhoMenu.php
@@ -0,0 +1,23 @@
+<header class="logado">
+        <nav class="navbar ajeitaNavBar">
+            <div class="iconemenu">
+                <input type="checkbox" id="check">
+                <label for="check">
+                <span class="material-symbols-outlined">menu</span>
+                </label>
+            </div>
+            <div class="navbar-logo">
+                <a href="inicio.html">
+                    <img src="../assets/img/LookingMenu.png" alt="Looking" class="logo">
+                </a>
+            </div>
+            <div class="pesquisa">
+                <form action="resultadobusca.html">
+                    <input type="text" name="pesquisa" placeholder="Buscar..."/>
+                    <button type="submit">
+                        <span class="material-symbols-outlined">search</span>
+                    </button>
+                </form>
+            </div>
+        </nav>
+    </header>
