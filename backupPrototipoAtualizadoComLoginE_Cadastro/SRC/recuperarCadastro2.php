7fbdb4760e3faaa98fd3a6df0914afaf9a925b31
ana julia <jeronymo2004@gmail.com>
2023-04-20 20:25:23 -0300

    Alteração arquivo recuperarcadastro2

--- backupPrototipoAtualizadoComLoginE_Cadastro/SRC/recuperarCadastro2.php
+++ backupPrototipoAtualizadoComLoginE_Cadastro/SRC/recuperarCadastro2.php
@@ -1,53 +1,66 @@
 <?php
-
-    include upload.php;
-
-    if(!isset($_SESSION["CPF"])) {
+    if(!isset($_SESSION["cpf"])) {
         session_start();
     }
+
+    require("conexao.php");
+    require("operacoes.php");
     
-    require "conexao.php";
-	require "operacoes.php";
-            
-    //dados do cadastro1.php
-    $Nascimento = $_POST["Nascimento"];
+    //dados do cadastro2.php
+    $nascimento = $_POST["nascimento"];
     //$genero = $_POST["genero"];
     //$imgpreview = $_POST["imgpreview"];
-    $Camiseta = $_POST["Camiseta"];
-    $Calca = $_POST["Calca"];
-    $Calcado = $_POST["Calcado"];
-    //$loja = $_POST["loja"];
+    $camiseta = $_POST["camiseta"];
+    $calca = $_POST["calca"];
+    $calcado = $_POST["calcado"];
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        if(isset($_FILES["imgpreview"])){
+            $img = $_FILES["imgpreview"];
+            $img_nome = $img["name"];
+            $img_tmp = $img["tmp_name"];
+            $img_ext = $img["type"];
+            $img_erro = $img["error"];
+        }
 
-    if(!empty(trim($Nascimento)) && !empty(trim($Camiseta)) && !empty(trim($Calca)) && !empty(trim($Calcado))){
+        if($img_erro == UPLOAD_ERR_OK){
+             $ImgDestino = "../uploads/".time().".jpg";
+             move_uploaded_file($img_tmp, $ImgDestino);
+             insert($ImgDestino);
+        }
+    }
+    
+
+    //
+    if(!empty($nascimento)  && !empty($camiseta) && !empty($calca) && !empty($calcado)){
 
         $conexao = getConexao();
 		
 		if($conexao){
 
             //dados do cadastro2.php
-            $Nome = $_SESSION["Nome"];
-            $Sobrenome = $_SESSION["Sobrenome"];
-            $CPF = $_SESSION["CPF"];
-            $Cidade = $_SESSION["Cidade"];
-            $Telefone = $_SESSION["Telefone"];
-            $Email = $_SESSION["Email"];
-            $Senha = $_SESSION["Senha"];
+            $nome = $_SESSION["nome"];
+            $sobrenome = $_SESSION["sobrenome"];
+            $cpf = $_SESSION["cpf"];
+            $cidade = $_SESSION["cidade"];
+            $tel = $_SESSION["tel"];
+            $email = $_SESSION["email"];
+            $senha = $_SESSION["senha"];
+            $ImgDestino = $_SESSION["ImgDestino"];
 
-			$cadastro = cadastrar_usuario($conexao, $Nascimento,  $Camiseta, $Calca, $Calcado, $Nome, $Sobrenome, $CPF, $Cidade, $Telefone, $Email, $Senha);
+
+			$cadastro = cadastrar_usuario($conexao, $nascimento, $camiseta, $calca, $calcado, $nome, $sobrenome, $cpf, $cidade, $tel, $email, $senha, $ImgDestino);//colocar nascimento também
 			if($cadastro){
-				$destino = "header('location: ../sucesso.html')";
+				header("location: sucesso.php");
 			}else{
-				$destino = "header('location: erro.php')";
-                $problema = "problema ao inserir no bd";
+				header("location: erro.php");
 			}
 		}else{
 			$erroConexao = mysqli_connect_error($conexao);
-            $problema = "Houve um erro na conexão do php com o banco de dados: " . $erroConexao;
+			echo "Houve um erro na conexão do php com o banco de dados: " . $erroConexao;
 		}
 	}else{
-		$destino = "header('location: erro.php')";
+		header("location: erro.php");
 	}
-    session_start();
-    echo $destino;
 ?>
