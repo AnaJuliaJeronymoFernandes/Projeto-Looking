64050a7c6402dea75f54d9f1004b62b1b0b95a02
ana julia <jeronymo2004@gmail.com>
2023-05-01 20:42:18 -0300

    procura aí

--- backupPrototipoAtualizadoComLoginE_Cadastro/SRC/inicio.php
+++ backupPrototipoAtualizadoComLoginE_Cadastro/SRC/inicio.php
@@ -16,10 +16,24 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Looking</title>
 </head>
+<body class="inicio">
 <?php
         include ("../include/cabecalhoMenu.php");
     ?>
-<body class="inicio">
+    
+    <main class="inicio">
+        <div class="sidebar ajeitaSidebar">
+            <div class="conteudo">
+            <img src="../assets/img/Bruna.jpg" class="image" alt="">
+            <h2>Bruna</h2>
+            <a href="meuperfil.php"><span class="material-symbols-outlined">person</span>Perfil</a>
+            <a href="favoritos.php"><span class="material-symbols-outlined">star</span>Favoritos</a>
+            <a href="cadastroloja.php"><span class="material-symbols-outlined">checkroom</span>Cadastrar Loja</a>
+            <a href=""><span class="material-symbols-outlined">settings</span>Configurações</a>
+            <a href="../SRC/logout.php" id="sair"><span class="material-symbols-outlined">logout</span>Sair</a>
+            </div>
+        </div>
+    
     
         <div id="feed">
 
@@ -110,6 +124,7 @@
             
         </div>
         
+    </main>
         <script src="../assets/js/script.js"></script>
         
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
--- backupPrototipoAtualizadoComLoginE_Cadastro/include/cabecalhoMenu.php
+++ backupPrototipoAtualizadoComLoginE_Cadastro/include/cabecalhoMenu.php
@@ -1,11 +1,3 @@
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css"/>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <title>Looking</title>
-</head>
 <header class="logado">
         <nav class="navbar ajeitaNavBar">
             <div class="iconemenu">
@@ -19,25 +11,12 @@
                     <img src="../assets/img/LookingMenu.png" alt="Looking" class="logo">
                 </a>
             </div>
-            <div class="pesquisa">
-                <form action="resultadobusca.php">
-                    <input type="text" name="pesquisa" placeholder="Buscar..."/>
-                    <button type="submit">
-                        <span class="material-symbols-outlined">search</span>
-                    </button>
-                </form>
+            
+            <div class="search-box">
+                <input type="text" class="search-text" placeholder="Pesquisar...">
+                <a href="" class="search-btn">
+                    <span class="material-symbols-outlined">search</span>
+                </a>
             </div>
         </nav>
     </header>
-    <main class="inicio">
-        <div class="sidebar ajeitaSidebar">
-            <div class="conteudo">
-            <img src="../assets/img/Bruna.jpg" class="image" alt="">
-            <h2>Bruna</h2>
-            <a href="meuperfil.php"><span class="material-symbols-outlined">person</span>Perfil</a>
-            <a href="favoritos.php"><span class="material-symbols-outlined">star</span>Favoritos</a>
-            <a href="cadastroloja.php"><span class="material-symbols-outlined">checkroom</span>Cadastrar Loja</a>
-            <a href=""><span class="material-symbols-outlined">settings</span>Configurações</a>
-            <a href="../SRC/logout.php" id="sair"><span class="material-symbols-outlined">logout</span>Sair</a>
-            </div>
-        </div>
