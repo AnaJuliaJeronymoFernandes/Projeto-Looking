23b067ffff239cbe97e2b51358d420b3c98cf31c
ana julia <jeronymo2004@gmail.com>
2023-05-01 16:55:35 -0300

    tentativa falha de colocar imagem de fundo no feed

--- backupPrototipoAtualizadoComLoginE_Cadastro/SRC/inicio.php
+++ backupPrototipoAtualizadoComLoginE_Cadastro/SRC/inicio.php
@@ -16,11 +16,11 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Looking</title>
 </head>
-<body class="inicio">
-    <?php
+<?php
         include ("../include/cabecalhoMenu.php");
     ?>
-
+<body class="inicio">
+    
         <div id="feed">
 
             <!-- PRIMEIRO POST -->
