1f56c6d9d216b382a6d3f6f7f87da5611ad8b7b0
ana julia <jeronymo2004@gmail.com>
2023-05-01 21:02:06 -0300

    não sei juntar o css no feed mim ajudem

--- backupPrototipoAtualizadoComLoginE_Cadastro/include/cabecalhoMenu.php
+++ backupPrototipoAtualizadoComLoginE_Cadastro/include/cabecalhoMenu.php
@@ -8,6 +8,36 @@
             </div>
             <div class="navbar-logo">
                 <a href="inicio.php">
+                    <img src="../assets/img/LookingMenu.png" alt="Looking" class="logoCabecalhoMenu">
+                </a>
+            </div>
+            <div class="search-box">
+                <input type="text" class="search-text" placeholder="Pesquisar...">
+                <a href="" class="search-btn">
+                    <span class="material-symbols-outlined">search</span>
+                </a>
+            </div>
+            <!-- <div class="pesquisa">
+                <form action="resultadobusca.php">
+                    <input type="text" name="pesquisa" placeholder="Buscar..."/>
+                    <button type="submit">
+                        <span class="material-symbols-outlined">search</span>
+                    </button>
+                </form>
+            </div> -->
+        </nav>
+    </header>
+
+<!-- <header class="logado">
+        <nav class="navbar ajeitaNavBar">
+            <div class="iconemenu">
+                <input type="checkbox" id="check">
+                <label for="check">
+                <span class="material-symbols-outlined">menu</span>
+                </label>
+            </div>
+            <div class="navbar-logo">
+                <a href="inicio.php">
                     <img src="../assets/img/LookingMenu.png" alt="Looking" class="logo">
                 </a>
             </div>
@@ -19,4 +49,4 @@
                 </a>
             </div>
         </nav>
-    </header>
+    </header> -->
